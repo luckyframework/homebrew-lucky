9c650a65a7f608016f8e7e381b8d6718f833599f
Paul Smith <paulcsmith0218@gmail.com>
2017-01-06 14:11:19 -0500

    New download URL

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,12 +1,12 @@
 class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/tasks"
-  url "https://github.com/luckyframework/tasks/archive/v0.1.0.tar.gz"
+  url "https://github.com/luckyframework/tasks/releases/download/v0.1.0/lucky"
   sha256 "cba961c3fa3c7c58a0b7dcd8ae063b65ebf0cc4437e104bfc368133e8c066d4f"
   depends_on "crystal-lang"
 
   def install
-    bin.install Dir["*"]
+    bin.install "lucky"
   end
 
   test do
