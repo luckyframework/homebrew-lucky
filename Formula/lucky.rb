3c39d88fb404fcc6d8d4c8665500e96c6c2ec3a8
Paul Smith <paulcsmith0218@gmail.com>
2017-01-06 12:53:05 -0500

    Add sha256 check

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,6 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/tasks"
   url "https://github.com/luckyframework/tasks/archive/v0.1.0.tar.gz"
+  sha256 "cba961c3fa3c7c58a0b7dcd8ae063b65ebf0cc4437e104bfc368133e8c066d4f"
   depends_on "crystal-lang"
 
   def install
