6d5afdfba3b8323b59f725713c1d3e09587e5000
Paul Smith <paulcsmith0218@gmail.com>
2017-01-06 14:14:59 -0500

    Add SHA instructions

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,6 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/tasks"
   url "https://github.com/luckyframework/tasks/releases/download/v0.1.0/lucky"
+  # shasum -a 256 {path to file}
   sha256 "52f41ce4495a7e5144cb790977431b2bfe38f3f019a423663706a6ca6c94fcd8"
   version "0.1.0"
   depends_on "crystal-lang"
