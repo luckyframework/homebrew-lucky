770a4523c80c8e6e5e7603ce1fc8ab1a6e6eb391
Paul Smith <paulcsmith0218@gmail.com>
2017-01-06 14:13:07 -0500

    sha256

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/tasks"
   url "https://github.com/luckyframework/tasks/releases/download/v0.1.0/lucky"
-  # sha256 "cba961c3fa3c7c58a0b7dcd8ae063b65ebf0cc4437e104bfc368133e8c066d4f"
+  sha256 "52f41ce4495a7e5144cb790977431b2bfe38f3f019a423663706a6ca6c94fcd8"
   version "0.1.0"
   depends_on "crystal-lang"
 
