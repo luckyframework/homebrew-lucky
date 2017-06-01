59d0b72ad18570910465e2b9cc83eef56f6da0b2
Paul Smith <paulcsmith0218@gmail.com>
2017-05-31 23:21:52 -0400

    Try moving the binary

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,11 +2,12 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/built_binaries/lucky-0.2.1"
-  sha256 "a931527758dde879b01b428a8a59cf2d6c174b1d9b9445d737053a75c0071068"
+  sha256 "bc617d6405c9d0aed58a147c11e82da90fdcc0e1008e15e06389cbbd68b7c041"
   version "0.2.1"
   depends_on "crystal-lang"
 
   def install
+    system "mv", "lucky-0.2.1", "lucky"
     bin.install "lucky"
   end
 
