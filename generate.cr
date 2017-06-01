e02b3adadf881087e848cb205468eb73d3248c07
Paul Smith <paulcsmith0218@gmail.com>
2017-05-31 21:48:17 -0400

    Output SHA in better format

--- generate.cr
+++ generate.cr
@@ -1,20 +1,27 @@
 require "colorize"
-require "lib/lucky_cli/src/lucky_cli/"
+require "./lib/lucky_cli/src/lucky_cli/version"
+
+version = LuckyCli::VERSION
 
 def run_command(command)
+  result = IO::Memory.new
   Process.run command,
     shell: true,
-    output: true,
+    output: result,
     error: true
+  result
 end
 
 puts "Installing shards"
 run_command "shards install"
 
 puts "Building lucky cli"
-run_command "crystal build --cross-compile --release lib/lucky_cli/src/lucky.cr"
+run_command "crystal build --release lib/lucky_cli/src/lucky.cr"
+run_command "mv lucky built_binaries/lucky-#{version}"
 
 puts "Done building"
 
 puts "Here is the SHA for the lucky binary:"
-run_command("shasum -a 256 lucky").colorize(:green)
+result = run_command "shasum -a 256 built_binaries/lucky-#{version}"
+
+puts result.to_s.split("  ").first.colorize(:green)
