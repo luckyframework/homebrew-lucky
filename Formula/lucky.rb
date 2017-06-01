6455a023581e25c03b5dd03cc8ad4aefc85f604e
Paul Smith <paulcsmith0218@gmail.com>
2017-05-31 23:23:27 -0400

    Fix SHA

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/built_binaries/lucky-0.2.1"
-  sha256 "bc617d6405c9d0aed58a147c11e82da90fdcc0e1008e15e06389cbbd68b7c041"
+  sha256 "06409e051e9f4fa9d52d0d2c22d43e9edf835c0637c3833c4345b904abc426b4"
   version "0.2.1"
   depends_on "crystal-lang"
 
