2ebd7f2599e24fef73ff7925d0a79b493b4e519c
Paul Smith <paulcsmith0218@gmail.com>
2017-09-01 15:16:45 -0400

    Bump to 0.4.1

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,13 +1,13 @@
 class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
-  url "https://github.com/luckyframework/homebrew-lucky/raw/master/built_binaries/lucky-0.4.0"
+  url "https://github.com/luckyframework/homebrew-lucky/raw/master/built_binaries/lucky-0.4.1"
   sha256 "6fcf77216deb789ce711a2b8ab37fc55cd489568fd2fe106bdec686603bc87ae"
-  version "0.4.0"
+  version "0.4.1"
   depends_on "crystal-lang"
 
   def install
-    system "mv", "lucky-0.4.0", "lucky"
+    system "mv", "lucky-0.4.1", "lucky"
     bin.install "lucky"
   end
 
