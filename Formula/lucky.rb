875288fc5916aca496678df61471ae1d3b594e06
Paul Smith <paulcsmith@users.noreply.github.com>
2018-05-12 11:58:48 -0400

    Add v0.10.0-rc2 (#4)

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,10 +1,16 @@
 class Lucky < Formula
-  desc "Task runner for Crystal"
+  desc "Task runner and Lucky project initializer for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.9.2.tar.gz"
   sha256 "d58ce3559eb09a3cb65fec82873ceafac1bac5097480932fbc869726f1f6a25e"
   version "0.9.2"
   depends_on "crystal-lang"
+
+  devel do
+    url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.10.0-rc2.tar.gz"
+    sha256 "3afe3f6a5d472286489615508affc5485622bcbe88fdcdeb6c4a8a8fb4278d84"
+    version "0.10.0-rc2"
+  end
 
   def install
     system "mkdir lib"
