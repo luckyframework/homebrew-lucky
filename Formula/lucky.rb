32bb73650bbe955ee3e993113ea9dfcaf8070f19
Paul Smith <paulcsmith0218@gmail.com>
2019-08-14 11:26:31 -0400

    Bump version to v0.17.0

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,9 +1,9 @@
 class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
-  url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.16.0.tar.gz"
-  sha256 "81a00f3f9388860cc3c58484213f3f80515964f1bd3b219c4ceeebcc6a589e4c"
-  version "0.16.0"
+  url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.17.0.tar.gz"
+  sha256 "f004d8ae1beb39be8cef5234a04c47c3f32c0a1571314f718149f31d60e592ce"
+  version "0.17.0"
   depends_on "crystal-lang"
 
   def install
