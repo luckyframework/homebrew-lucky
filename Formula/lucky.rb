a6739804b3b1dfa2804790ea82dc03910590c0e3
Paul Smith <paulcsmith0218@gmail.com>
2020-06-16 16:15:58 -0400

    Get SHA matched up

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.22.0.tar.gz"
-  sha256 "4d8515bdbbc97e129234125c87fcbeb6c9c8bf2069f2d9e11d6fc8a7f9115da0"
+  sha256 "b88586fdf5b92126aa1fea893db215fcecc1b79e392b947f8cc121b34524ff11"
   version "0.22.0"
   depends_on "crystal-lang"
 
