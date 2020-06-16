74b2b78e3926ae99b6baeacd4544fc0089a5f4be
Paul Smith <paulcsmith0218@gmail.com>
2020-06-16 15:52:50 -0400

    Fix v0.22.0 by moving 'future' shard

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.22.0.tar.gz"
-  sha256 "aaa5fbd9df0253c4320d7c3d9665a4999980d1e1c8983c6da22b28b0dd48b242"
+  sha256 "d1da3d1f6f3b0b7d7f32f39954744d79a77f21094789f1d5205f398dc290e120"
   version "0.22.0"
   depends_on "crystal-lang"
 
