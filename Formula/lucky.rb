e8c44207353da56a137874f71dcd04917a0e0af1
Paul Smith <paulcsmith0218@gmail.com>
2020-06-16 16:02:59 -0400

    Actually add future

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,20 +1,23 @@
+# frozen_string_literal: true
+
 class Lucky < Formula
-  desc "Task runner for Crystal"
-  homepage "http://github.com/luckyframework/cli"
-  url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.22.0.tar.gz"
-  sha256 "d1da3d1f6f3b0b7d7f32f39954744d79a77f21094789f1d5205f398dc290e120"
-  version "0.22.0"
-  depends_on "crystal-lang"
+  desc 'Task runner for Crystal'
+  homepage 'http://github.com/luckyframework/cli'
+  url 'https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.22.0.tar.gz'
+  sha256 'd1da3d1f6f3b0b7d7f32f39954744d79a77f21094789f1d5205f398dc290e120'
+  version '0.22.0'
+  depends_on 'crystal-lang'
 
   def install
-    system "mkdir lib"
-    system "mv teeplate lib/"
-    system "mv lucky_cli lib/"
-    system "crystal build lib/lucky_cli/src/lucky.cr"
-    bin.install "lucky"
+    system 'mkdir lib'
+    system 'mv future lib/'
+    system 'mv teeplate lib/'
+    system 'mv lucky_cli lib/'
+    system 'crystal build lib/lucky_cli/src/lucky.cr'
+    bin.install 'lucky'
   end
 
   test do
-    system "{bin}/lucky", "--help"
+    system '{bin}/lucky', '--help'
   end
 end
