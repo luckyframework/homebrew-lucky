bc659f244d5e6e959cf714473b2d6de98654a3f5
Paul Smith <paulcsmith0218@gmail.com>
2020-06-17 10:44:28 -0400

    Update

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.22.0.tar.gz"
-  sha256 "688aadff6928bcffec5a4aebc7f430bd16309f654777a0adb62d3b95bd43a73c"
+  sha256 "301b4dc34f0e28f61d0b7890abbff4397bd0620d3307b487c9d840eb393c153e"
   version "0.22.0"
   depends_on "crystal"
 
