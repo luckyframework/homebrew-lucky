51628a6e464ef2ae5a2b542bfda053626705b642
Jeremy Woertink <jeremywoertink@gmail.com>
2021-04-12 10:58:49 -0700

    Fixing install to ensure lucky_task is built

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
   url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.27.0.tar.gz"
-  sha256 "e3ffb19c17c884e30e2c29892c7dc981ed5ad53ddcd0129f3c7583344751b62f"
+  sha256 "327458998a2a3aeb0b722bf9b2854b4f08659d28c6dbfdd16119fef4fcd705bc"
   version "0.27.0"
   depends_on "crystal"
 
@@ -10,6 +10,7 @@ class Lucky < Formula
     system "mkdir lib"
     system "mv future lib/"
     system "mv teeplate lib/"
+    system "mv lucky_task lib/"
     system "mv lucky_cli lib/"
     system "crystal build lib/lucky_cli/src/lucky.cr"
     bin.install "lucky"
