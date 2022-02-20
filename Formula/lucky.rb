b7c30d3b58bcdfffbcf4553d78354dcd7611c774
Liberatys <nick.flueckiger@renuo.ch>
2022-02-20 16:53:46 +0100

    Update lucky.rb

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -1,7 +1,7 @@
 class Lucky < Formula
   desc "Task runner for Crystal"
   homepage "http://github.com/luckyframework/cli"
-  url "https://github.com/luckyframework/homebrew-lucky/raw/master/tarballs/lucky-0.29.0.tar.gz"
+  url "https://github.com/luckyframework/homebrew-lucky/raw/main/tarballs/lucky-0.29.0.tar.gz"
   sha256 "0e2e52f051d1d461991a340635806440f0630a960bc17005ce00fa2ac03a6146"
   version "0.29.0"
   depends_on "crystal"
