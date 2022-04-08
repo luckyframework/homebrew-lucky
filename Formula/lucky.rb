cba49aa9b250793d731c9d2b51a137eefd336d3c
Jeremy Woertink <jeremywoertink@gmail.com>
2022-04-07 22:05:59 -0700

    future isn't required here, and was causing the forumla to fail

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,13 +2,12 @@ class Lucky < Formula
   desc "CLI installer for Lucky Framework"
   homepage "https://github.com/luckyframework/lucky_cli"
   url "https://raw.githubusercontent.com/luckyframework/homebrew-lucky/main/tarballs/lucky-0.30.0.tar.gz"
-  sha256 "e7563eada76ffcc1cf1845f5c5a70fa274a494e66df3c1e0fef85342721cd71f"
+  sha256 "37d7f7dfa89b82eabd671148e4af0508b5b0fef56da2a19b7e7b0514a68e3c9c"
   version "0.30.0"
   depends_on "crystal"
 
   def install
     system "mkdir lib"
-    system "mv future lib/"
     system "mv teeplate lib/"
     system "mv lucky_task lib/"
     system "mv lucky_cli lib/"
