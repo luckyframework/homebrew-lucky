f52c13a6a1d1a47486c999ecf70d73514f4eaaa1
Jeremy Woertink <jeremywoertink@gmail.com>
2022-04-07 22:12:09 -0700

    Giving this sha a chance

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,7 +2,7 @@ class Lucky < Formula
   desc "CLI installer for Lucky Framework"
   homepage "https://github.com/luckyframework/lucky_cli"
   url "https://raw.githubusercontent.com/luckyframework/homebrew-lucky/main/tarballs/lucky-0.30.0.tar.gz"
-  sha256 "37d7f7dfa89b82eabd671148e4af0508b5b0fef56da2a19b7e7b0514a68e3c9c"
+  sha256 "208b60d55eceb5957c93ddb427027942641ff251ce696239d03e9e761d012443"
   version "0.30.0"
   depends_on "crystal"
 
