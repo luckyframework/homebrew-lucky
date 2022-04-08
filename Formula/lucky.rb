2baa0834a6d14b0f4f0cd3ba8364e68267ded666
Jeremy Woertink <jeremywoertink@gmail.com>
2022-04-08 07:26:58 -0700

    Nox is also required to build CLI now.

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,12 +2,13 @@ class Lucky < Formula
   desc "CLI installer for Lucky Framework"
   homepage "https://github.com/luckyframework/lucky_cli"
   url "https://raw.githubusercontent.com/luckyframework/homebrew-lucky/main/tarballs/lucky-0.30.0.tar.gz"
-  sha256 "208b60d55eceb5957c93ddb427027942641ff251ce696239d03e9e761d012443"
+  sha256 "609188dc195ac0611bef1bdada626231795f4872dada27d2e7c366ae7a5a6285"
   version "0.30.0"
   depends_on "crystal"
 
   def install
     system "mkdir lib"
+    system "mv nox lib/"
     system "mv teeplate lib/"
     system "mv lucky_task lib/"
     system "mv lucky_cli lib/"
