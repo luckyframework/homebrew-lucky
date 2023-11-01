dc6768ee761fea416606a3004a192eb162c23f98
Jeremy Woertink <jeremywoertink@gmail.com>
2023-10-31 19:06:25 -0700

    Forgot to remove teeplate. Fixes #25

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -2,14 +2,14 @@ class Lucky < Formula
   desc "CLI installer for Lucky Framework"
   homepage "https://github.com/luckyframework/lucky_cli"
   url "https://raw.githubusercontent.com/luckyframework/homebrew-lucky/main/tarballs/lucky-1.1.0.tar.gz"
-  sha256 "8ecc09bbccd04b4041f4ebaf672888eaf2548752aa8e7dd2ac1443650443cb46"
+  sha256 "6fad3305c5d842a2a4efe59d8f9e51ec1ebf38e4ac7633dbcc88c8b855cd5be2"
   version "1.1.0"
   depends_on "crystal"
 
   def install
     system "mkdir lib"
     system "mv nox lib/"
-    system "mv teeplate lib/"
+    system "mv lucky_template lib/"
     system "mv lucky_task lib/"
     system "mv lucky_cli lib/"
     system "crystal build lib/lucky_cli/src/lucky.cr"
