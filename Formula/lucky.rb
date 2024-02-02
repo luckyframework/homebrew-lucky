71e8b7533729a63113cd746c4a1b87607c759665
mdwagner <13472976+mdwagner@users.noreply.github.com>
2024-02-02 01:02:03 +0000

    release: bump to 1.1.0-test2

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -3,10 +3,10 @@ class Lucky < Formula
   desc "A Crystal command-line tool for generating new Lucky Web Applications"
   homepage "https://github.com/luckyframework/lucky_cli"
   license "MIT"
-  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.1.0.zip"
-  sha256 "39b4528feef22983e96c748591cc8102c6a5f315ce4c1f56e4c60b8e21af108e"
+  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.1.0-test2.zip"
+  sha256 "a02c7d3a1d9f962d4c4b4715ce08b33b7e35cf876c95c9d6b09da73dd30574d4"
   head "https://github.com/luckyframework/lucky_cli.git", branch: "main"
-  version "1.1.0"
+  version "1.1.0-test2"
 
   depends_on "crystal" => :build
   depends_on "git" => :build
