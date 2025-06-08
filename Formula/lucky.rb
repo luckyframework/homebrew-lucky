c80f7ea38f1ada5fb583021f56a823accb5fbe30
Xavi Ablaza <xavi@hostari.com>
2025-06-08 13:29:58 -0500

    bump to 1.4.0

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -3,10 +3,10 @@ class Lucky < Formula
   desc "A Crystal command-line tool for generating new Lucky Web Applications"
   homepage "https://github.com/luckyframework/lucky_cli"
   license "MIT"
-  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.3.0.zip"
-  sha256 "cc9e02da676fa1aec601862fb1e6b65b8a44d2f68df2ecebdce4a5a2a76ef8c7"
+  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.4.0.zip"
+  sha256 "d5f7fcc0aa59dd1563608e104d196863853e6d6291e54e8c1d5e8f9db474d37a"
   head "https://github.com/luckyframework/lucky_cli.git", branch: "main"
-  version "1.3.0"
+  version "1.4.0"
 
   depends_on "crystal" => :build
   depends_on "git" => :build
@@ -17,6 +17,6 @@ class Lucky < Formula
   end
 
   test do
-    assert_equal "1.3.0", shell_output("#{bin}/lucky --version").strip
+    assert_equal "1.4.0", shell_output("#{bin}/lucky --version").strip
   end
 end
