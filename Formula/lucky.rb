2fce042224e65a2445926278598090bedf1064b3
Jeremy Woertink <jeremywoertink@gmail.com>
2025-06-20 18:33:57 -0700

    Update forumla (#37)

--- Formula/lucky.rb
+++ Formula/lucky.rb
@@ -3,10 +3,10 @@ class Lucky < Formula
   desc "A Crystal command-line tool for generating new Lucky Web Applications"
   homepage "https://github.com/luckyframework/lucky_cli"
   license "MIT"
-  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.3.0.zip"
-  sha256 "cc9e02da676fa1aec601862fb1e6b65b8a44d2f68df2ecebdce4a5a2a76ef8c7"
+  url "https://github.com/luckyframework/lucky_cli/archive/refs/tags/v1.4.1.zip"
+  sha256 "5182ef001012142ff6623be25d09686c8da22a4088bdcc55795ba90d7489186d"
   head "https://github.com/luckyframework/lucky_cli.git", branch: "main"
-  version "1.3.0"
+  version "1.4.1"
 
   depends_on "crystal" => :build
   depends_on "git" => :build
@@ -17,6 +17,6 @@ class Lucky < Formula
   end
 
   test do
-    assert_equal "1.3.0", shell_output("#{bin}/lucky --version").strip
+    assert_equal "1.4.1", shell_output("#{bin}/lucky --version").strip
   end
 end
